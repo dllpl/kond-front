6a375a2a2d553ad816e668d2181ecaae76346fec
Nikita Ivanov <nikita03.1998@mail.ru>
2025-01-27 11:30:34 +0300

    Правка

--- pages/catalog/[...slug].vue
+++ pages/catalog/[...slug].vue
@@ -23,13 +23,18 @@ const route = useRoute();
 const {public: config} = useRuntimeConfig();
 
 let uri = route.params?.slug ? route.params.slug.join('/') : '';
+
+console.log(uri)
+
 const {data} = await useFetch(config.backOptions.api + '/products-categories/' + uri);
+
+console.log(data.value.breadcrumbs)
 
 const breadcrumbs = [
   {
     name: 'Каталог',
     uri: 'catalog',
   },
-  ...(data.value?.breadcrumbs ? data.value.breadcrumbs : {}),
+  ...data.value.breadcrumbs,
 ]
 </script>
