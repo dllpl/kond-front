38da6b7a9ef6148944384cc076aab0a9a088659e
Nikita Ivanov <nikita03.1998@mail.ru>
2025-01-27 11:15:30 +0300

    Правка

--- pages/catalog/[...slug].vue
+++ pages/catalog/[...slug].vue
@@ -2,17 +2,17 @@
 
   <div class="">
     <section>
-      <ElementsBreadcrumb class="wrapper-container py-4" :data="breadcrumbs" />
+      <ElementsBreadcrumb class="wrapper-container py-4" :data="breadcrumbs"/>
     </section>
 
     <main class="wrapper-container pb-16">
       <template v-if="!data.product">
         <SectionCatalogList :data="data.data"
-          :class="data.data[0].children.length === 0 || data.products.length === 0 ? 'mb-0' : 'mb-20'" />
-        <SectionProductList :data="data.products" v-if="data.products.length" />
+                            :class="data.data[0].children.length === 0 || data.products.length === 0 ? 'mb-0' : 'mb-20'"/>
+        <SectionProductList :data="data.products" v-if="data.products.length"/>
       </template>
 
-      <SectionProductPage v-else :product="data.product" />
+      <SectionProductPage v-else :product="data.product"/>
     </main>
   </div>
 </template>
@@ -20,16 +20,16 @@
 
 <script setup>
 const route = useRoute();
-const { public: config } = useRuntimeConfig();
+const {public: config} = useRuntimeConfig();
 
 let uri = route.params?.slug ? route.params.slug.join('/') : '';
-const { data } = await useFetch(config.backOptions.api + '/products-categories/' + uri);
+const {data} = await useFetch(config.backOptions.api + '/products-categories/' + uri);
 
 const breadcrumbs = [
   {
     name: 'Каталог',
     uri: 'catalog',
   },
-  ...data.value.breadcrumbs
+  ...(data.value?.breadcrumbs || {})
 ]
 </script>
