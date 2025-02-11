2e8975ff4c97d96d93afbe9271b867feece093ce
dllpl <nick.iv.dev@gmail.com>
2025-02-11 19:56:28 +0300

    Правка

--- pages/index.vue
+++ pages/index.vue
@@ -13,17 +13,18 @@
         <SectionAdvantages class="lg:hidden" />
       </div>
 
-      <ClientOnly>
-        <ElementsSliderProduct v-if="productsOffers" :data="productsOffers.data" title="Спецпредложения"
-          class="col-span-2 lg:grid-cols-1  relative" />
 
-        <SectionProductTop v-if="productsSets" :data="productsSets.data" class="col-span-2 lg:grid-cols-1 "
-          title="Популярные категории" />
+      <ClientOnly>
+          <LazyElementsSliderProduct v-if="productsOffersStatus === 'success'" :data="productsOffers.data" title="Спецпредложения"
+                                 class="col-span-2 lg:grid-cols-1 relative"/>
+          <LazySectionProductTop v-if="productsSetsStatus === 'success'" :data="productsSets.data" class="col-span-2 lg:grid-cols-1 "
+                             title="Популярные категории" />
       </ClientOnly>
+
 
       <SectionPromoPrice class="col-span-2 lg:grid-cols-1 " />
       <ClientOnly>
-        <LazyElementsSliderProduct v-if="productsNew" :data="productsNew.data" title="Новинки"
+        <LazyElementsSliderProduct v-if="productsNewStatus === 'success'" :data="productsNew.data" title="Новинки"
           class="col-span-2 lg:grid-cols-1  relative" />
       </ClientOnly>
       <SectionAdvantages class="hidden lg:block" />
@@ -40,7 +41,7 @@ const { public: config } = useRuntimeConfig();
 const { data: banners } = await useFetch(config.backOptions.api + '/banners');
 const { data: categories } = await useFetch(config.backOptions.api + '/products-categories');
 
-const { data: productsNew } = await useFetch(config.backOptions.api + '/products/new', { server: false });
-const { data: productsOffers } = await useFetch(config.backOptions.api + '/products/spec', { server: false });
-const { data: productsSets } = await useFetch(config.backOptions.api + '/products-sets', { server: false });
+const { status: productsNewStatus, data: productsNew } = await useFetch(config.backOptions.api + '/products/new', { server: false, lazy: true });
+const { status: productsOffersStatus, data: productsOffers } = await useFetch(config.backOptions.api + '/products/spec', { server: false, lazy: true });
+const { status: productsSetsStatus, data: productsSets } = await useFetch(config.backOptions.api + '/products-sets', { server: false, lazy: true });
 </script>
