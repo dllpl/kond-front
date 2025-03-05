06029d46840198f8e5c88342c0675fda85a3d3fc
dllpl <nick.iv.dev@gmail.com>
2025-03-05 22:24:52 +0300

    logo to webp

--- components/section/ProductList.vue
+++ components/section/ProductList.vue
@@ -3,11 +3,15 @@
 
     <h2 class="sr-only">Товары</h2>
     <div
-      class=" grid grid-cols-6 border-l border-gray-200 mx-0 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1">
-      <LazySectionProductCard v-for="product in data" :key="product.id" :product="product"
-        class="border-b border-r border-t border-gray-200" />
+        class="grid grid-cols-6 border-l border-gray-200 mx-0 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1">
+      <LazySectionProductCard
+          v-for="(product, i) in data"
+          :key="i"
+          :product="product"
+          :index="i"
+          class="border-b border-r border-t border-gray-200"
+      />
     </div>
-
   </div>
 </template>
 
