1d9436c941b2bf01cfb1bb55885e26f4fdf07ecb
Nikita Ivanov <nikita03.1998@mail.ru>
2025-04-09 10:54:33 +0300

    Финал

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: {enabled: false},
-  ssr: true,
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: false},
+    ssr: true,
 
   runtimeConfig: {
     public: {
@@ -12,137 +12,137 @@ export default defineNuxtConfig({
     },
   },
 
-  nitro: {
-      routeRules: {
-        '/**': {
-          headers: {
-            'X-Frame-Options': 'SAMEORIGIN',
-            'X-Content-Type-Options': 'nosniff',
-            'Referrer-Policy': 'strict-origin-when-cross-origin',
-          }
-        },
-      }
-  },
+    nitro: {
+        routeRules: {
+            '/**': {
+                headers: {
+                    'X-Frame-Options': 'SAMEORIGIN',
+                    'X-Content-Type-Options': 'nosniff',
+                    'Referrer-Policy': 'strict-origin-when-cross-origin',
+                }
+            },
+        }
+    },
 
-  routeRules: {
-    "/": {isr: 300, headers: {"Cache-Control": "public, max-age=300"}},
-    "/_nuxt/**": {headers: {"Cache-Control": "public, max-age=31536000, immutable"}},
-    "/static/**": {headers: {"Cache-Control": "public, max-age=86400"}},
+    routeRules: {
+        "/": {isr: 300, headers: {"Cache-Control": "public, max-age=300"}},
+        "/_nuxt/**": {headers: {"Cache-Control": "public, max-age=31536000, immutable"}},
+        "/static/**": {headers: {"Cache-Control": "public, max-age=86400"}},
 
-    "/api/user/**": {headers: {"Cache-Control": "no-store, no-cache, must-revalidate"}},
-    "/api/orders/**": {headers: {"Cache-Control": "no-store, no-cache, must-revalidate"}},
+        "/api/user/**": {headers: {"Cache-Control": "no-store, no-cache, must-revalidate"}},
+        "/api/orders/**": {headers: {"Cache-Control": "no-store, no-cache, must-revalidate"}},
 
-    "/api/**": {swr: 300, headers: {"Cache-Control": "public, max-age=300"}},
+        "/api/**": {swr: 300, headers: {"Cache-Control": "public, max-age=300"}},
 
-    "/lk/**": {headers: {"Cache-Control": "private, max-age=0, no-cache,no-store, must-revalidate"}},
-  },
+        "/lk/**": {headers: {"Cache-Control": "private, max-age=0, no-cache,no-store, must-revalidate"}},
+    },
 
-  icon: {
-    provider: "server",
-    clientBundle: {
-      scan: true,
-    }
-  },
+    icon: {
+        provider: "server",
+        clientBundle: {
+            scan: true,
+        }
+    },
 
-  // experimental: {
+    // experimental: {
     // payloadExtraction: true,
     // viewTransition: true, // Плавные переходы между страницами
-  // },
+    // },
 
-  modules: [
-    '@nuxt/icon',
-    '@pinia/nuxt',
-    '@nuxtjs/google-fonts',
-    '@nuxtjs/seo',
-    'yandex-metrika-module-nuxt3',
-    'nuxt-headlessui',
-  ],
+    modules: [
+        '@nuxt/icon',
+        '@pinia/nuxt',
+        '@nuxtjs/google-fonts',
+        '@nuxtjs/seo',
+        'yandex-metrika-module-nuxt3',
+        'nuxt-headlessui',
+    ],
 
-  css: ['~/assets/css/main.css'],
+    css: ['~/assets/css/main.css'],
 
-  headlessui: {
-    prefix: ''
-  },
-
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    headlessui: {
+        prefix: ''
     },
-  },
 
-  site: {
-    indexable: true,
-    url: '',
-    name: 'Всё для кондитера',
-    defaultLocal: 'ru',
-  },
-
-  sitemap: {
-    defaults: {
-      changefreq: 'daily',
-      priority: 0.5,
-      lastmod: new Date().toISOString(),
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
     },
-    sources: [process.env.API_URL + '/sitemap/generate'],
-  },
 
-  serverMiddleware: [
-    { path: '/', handler: '~/server/middleware/redirects.ts' }
-  ],
-
-  app: {
-    head: {
-      htmlAttrs: {
-        lang: 'ru'
-      },
-      charset: 'utf-8',
-      title: 'Всё для кондитера',
-      viewport: 'width=device-width, initial-scale=1',
-      templateParams: {
-        separator: '-'
-      },
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      ],
-      script: [
-        // {src: 'https://www.googletagmanager.com/gtag/js?id=G-5H3XLN9HST', async: true},
-      ]
+    site: {
+        indexable: true,
+        url: '',
+        name: 'Всё для кондитера',
+        defaultLocal: 'ru',
     },
-  },
 
-  router: {
-    prefetchLinks: false,
-    options: {
-      scrollBehaviorType: 'smooth'
+    sitemap: {
+        defaults: {
+            changefreq: 'daily',
+            priority: 0.5,
+            lastmod: new Date().toISOString(),
+        },
+        sources: [process.env.API_URL + '/sitemap/generate'],
     },
-  },
 
-  googleFonts: {
-    families: {
-      'Open Sans': '400..600',
-      Montserrat: '400..600',
+    serverMiddleware: [
+        {path: '/', handler: '~/server/middleware/redirects.ts'}
+    ],
+
+    app: {
+        head: {
+            htmlAttrs: {
+                lang: 'ru'
+            },
+            charset: 'utf-8',
+            title: 'Всё для кондитера',
+            viewport: 'width=device-width, initial-scale=1',
+            templateParams: {
+                separator: '-'
+            },
+            link: [
+                {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+            ],
+            script: [
+                // {src: 'https://www.googletagmanager.com/gtag/js?id=G-5H3XLN9HST', async: true},
+            ]
+        },
     },
-    overwriting: true,
-    display: 'swap',
-    preload: true,
-    prefetch: true,
-    preconnect: true,
-    useStylesheet: true,
-  },
 
-  yandexMetrika: {
-    id: process.env.YANDEX_METRIKA_ID || '65355439',
-    webvisor: true,
-    consoleLog: false,
-    defer: true
-  },
+    router: {
+        prefetchLinks: false,
+        options: {
+            scrollBehaviorType: 'smooth'
+        },
+    },
 
-  schemaOrg: {
-    defaults: false,
-  },
+    googleFonts: {
+        families: {
+            'Open Sans': '400..600',
+            Montserrat: '400..600',
+        },
+        overwriting: true,
+        display: 'swap',
+        preload: true,
+        prefetch: true,
+        preconnect: true,
+        useStylesheet: true,
+    },
 
-  devServer: {
-    host: '127.0.0.1',
-  },
+    yandexMetrika: {
+        id: process.env.YANDEX_METRIKA_ID || '65355439',
+        webvisor: true,
+        consoleLog: false,
+        defer: true
+    },
+
+    schemaOrg: {
+        defaults: false,
+    },
+
+    devServer: {
+        host: '127.0.0.1',
+    },
 })
