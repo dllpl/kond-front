a420fd5ae37e0e2e1d13f68e42de612471665e48
Nikita Ivanov <nikita03.1998@mail.ru>
2025-05-29 15:31:24 +0300

    gtag and bread

--- components/elements/Breadcrumb.vue
+++ components/elements/Breadcrumb.vue
@@ -3,17 +3,17 @@
         <ol role="list" class="flex items-center space-x-1 overflow-x-scroll no-scrollbar">
             <li>
                 <NuxtLink to="/" class="flex items-center text-gray-400 hover:text-gray-500 transition-all">
-                    <Icon name="hugeicons:home-05" class="h-5 w-5 flex-shrink-0" aria-hidden="true" />
+                    <Icon name="hugeicons:home-05" class="h-5 w-5 flex-shrink-0" aria-hidden="true"/>
                     <span class="sr-only">Главная</span>
                 </NuxtLink>
             </li>
             <li class="flex items-center whitespace-nowrap" v-for="(page, i) in data" :key="i">
-                <Icon name="hugeicons:arrow-right-01" class="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                <Icon name="hugeicons:arrow-right-01" class="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true"/>
                 <span v-if="data.length - 1 === i" class="ml-1 text-sm font-medium text-gray-400 ">
                     {{ page.name }}
                 </span>
                 <NuxtLink v-else :to="`/${page.uri}`"
-                    class="ml-1 text-sm font-medium text-gray-400 hover:text-gray-500 transition-all">
+                          class="ml-1 text-sm font-medium text-gray-400 hover:text-gray-500 transition-all">
                     {{ page.name }}
                 </NuxtLink>
             </li>
@@ -27,20 +27,32 @@ const props = defineProps({
 })
 
 useSchemaOrg([
-  defineBreadcrumb({
-    itemListElement: () => {
-        return props.data.map((item, i) => {
-            return {
-                "@type": "ListItem",
-                position: i + 1,
-                item: {
-                    "@type": "Thing",
-                    "@id": `https://dljakonditera.ru/${item.uri}`,
-                    name: item.name
-                }
-            }
-        })
-    },
-  })
+    defineBreadcrumb({
+        itemListElement: () => {
+            return [
+                {
+                    "@type": "ListItem",
+                    position: 1,
+                    name: "Главная",
+                    item: {
+                        "@type": "Thing",
+                        "@id": "https://dljakonditera.ru",
+                        name: "Главная"
+                    }
+                },
+                ...props.data.map((item, i) => {
+                    return {
+                        "@type": "ListItem",
+                        position: i + 2,
+                        item: {
+                            "@type": "Thing",
+                            "@id": `https://dljakonditera.ru/${item.uri}`,
+                            name: item.name
+                        }
+                    }
+                })
+            ]
+        },
+    })
 ])
 </script>
